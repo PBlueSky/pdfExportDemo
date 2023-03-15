b1286bfe4cace0ad3ed7ff96bc66522fa06f1407
Administrator <admin@example.com>
2023-03-15 18:34:45 +0800

    修改

--- src/main/java/com/utils/pdf/Main.java
+++ src/main/java/com/utils/pdf/Main.java
@@ -36,7 +36,6 @@ public class Main {
         }
     }
 
-
     private static String parseThymeleafTemplate() {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setSuffix(".html");
